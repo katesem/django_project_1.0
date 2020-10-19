505b5c7bd49a878fd941cbf4518efa34af1c9026
katesem <katewhite922@gmail.com>
2020-10-19 11:37:56 +0300

    Created base and homepage,  some changes in project structure

--- learning_hub/urls.py
+++ learning_hub/urls.py
@@ -1,5 +1,8 @@
-from django.contrib import admin
 from django.urls import path, include
+from . import views
 
 
-urlpatterns = []
+urlpatterns = [
+    path('', views.homepage, name='homepage'),
+    #path('sign_up', views.sign_up,name='sign_up')
+]
