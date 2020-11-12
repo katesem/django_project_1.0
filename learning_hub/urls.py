92a9f15e1707c25b0ca01e8a7b63b1d443960b07
katesem <katewhite922@gmail.com>
2020-11-12 16:05:23 +0200

    Applied some changes in templates, started work on 'quiz taking' algorithm
    + added contact page view

--- learning_hub/urls.py
+++ learning_hub/urls.py
@@ -5,6 +5,7 @@ from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     
+    #Common urls 
     path('', views.homepage, name='homepage'),
     path('about', views.about_page, name='about'),
     path('create_account', views.create_account, name='create_acc'),
@@ -13,12 +14,19 @@ urlpatterns = [
     path('my_account', views.my_account, name = 'my_account'),
     path('accounts/login/', views.password_redirect),
     path('contact', views.contact),
+    
+    #urls related to quiz creation 
     path('create_question', views.create_question, name = 'c_question'),
     path('create_quiz', views.create_quiz, name = 'c_quiz'),
     path('create_topic', views.create_topic, name = 'c_topic'),
     
+    #urls related to topic display
     path('topic/<int:topic_id>', views.topic_by_id, name = 'get_topic_by_id'),
     path('all_topics', views.all_topics, name = "topics"),
+    
+    # urls related to quiz taking
+    path('take_a_quiz/<int:quiz_id>', views.take_a_quiz, name = 'take_a_quiz'),
+    path('quiz_results', views.quiz_results, name = 'quiz_results'),
     
     # Password reset functionality
     path('reset_password', auth_views.PasswordResetView.as_view(), name="reset_password"),
